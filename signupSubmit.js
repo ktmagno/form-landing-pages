2537a2a6a982c6dcc15ee2bc2c0ec6b94842d79c
ktmagno <160462399+ktmagno@users.noreply.github.com>
2024-04-09 14:02:29 +0800

    Create signupSubmit.js
    jQuery for password confirmation before submit

---
+++ signupSubmit.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+  $("#btn-signup").css("opacity", "0.5");
+
+  //button disable, enable
+  $("#cpassword").on("keyup", function () {
+    if ($("#cpassword").val() == $("#ipassword").val()) {
+      $("#btn-signup").prop("disabled", false);
+    } else {
+      $("#btn-signup").prop("disabled", true);
+    }
+  });
+
+  $("#ipassword").on("keyup", function () {
+    if ($("#ipassword").val() == $("#cpassword").val()) {
+      $("#btn-signup").prop("disabled", false);
+    } else {
+      $("#btn-signup").prop("disabled", true);
+    }
+  });
+
+  //button color
+  $("#ipassword").on("keyup", function () {
+    if ($("#ipassword").val() == $("#cpassword").val()) {
+      $("#btn-signup").css("opacity", "1");
+    } else $("#btn-signup").css("opacity", "0.5");
+  });
+
+  $("#cpassword").on("keyup", function () {
+    if ($("#cpassword").val() == $("#ipassword").val()) {
+      $("#btn-signup").css("opacity", "1");
+    } else $("#btn-signup").css("opacity", "0.5");
+  });
+
+  //message
+  $("#ipassword").on("keyup", function () {
+    if ($("#ipassword").val() == $("#cpassword").val()) {
+      $("#message").text("Passwords match").css("color", "green");
+    } else $("#message").text("Passwords do not match").css("color", "red");
+  });
+
+  $("#cpassword").on("keyup", function () {
+    if ($("#cpassword").val() == $("#ipassword").val()) {
+      $("#message").text("Passwords match!").css("color", "green");
+    } else $("#message").text("Passwords do not match").css("color", "red");
+  });
+});
