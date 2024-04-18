cfe31d4265817eb9f47870cc2949003d151ad080
Karen Magno <karenmagno96@gmail.com>
2024-04-18 13:14:03 +0800

    Added email pattern

--- signupSubmit.js
+++ signupSubmit.js
@@ -19,31 +19,7 @@ $(document).ready(function () {
   //password validation
   $("#ipassword, #cpassword").on("keyup", function () {
     if ($("#ipassword").val() == $("#cpassword").val()) {
-      $("#message").text("Passwords match").css("color", "green");
-    } else $("#message").text("Passwords do not match").css("color", "red");
+      $("#pw-message").text("Passwords match!").css("color", "green");
+    } else $("#pw-message").text("Passwords do not match").css("color", "red");
   });
-
-  //email validation
-  const validateEmail = (email) => {
-    return email.match(
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-  };
-
-  const validate = () => {
-    const $result = $("#result");
-    const email = $("#email").val();
-    $result.text("");
-
-    if (validateEmail(email)) {
-      $result.text(email + " is valid.");
-      $result.css("color", "green");
-    } else {
-      $result.text(email + " is invalid.");
-      $result.css("color", "red");
-    }
-    return false;
-  };
-
-  $("#email").on("input", validate);
 });
